7d6b40d985c3bfbbbd9c0aaf62439fd5d395073e
marquesaline <alinem_oliveira@yahoo.com>
2023-04-03 20:44:41 -0300

    criação do model e da migration

--- consultas/models.py
+++ consultas/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    criacao = models.DateTimeField(auto_now_add=True)
+    atualizacao = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Profissional(BaseModel):
+    nome = models.CharField(max_length=100, blank=False, verbose_name='Nome')
+    prof_nome_social = models.CharField(max_length=100, blank=True, verbose_name='Nome social do profissional')
+    especialidade = models.CharField(max_length=100, blank=True, verbose_name='Especialidade')
+
+    class Meta:
+        verbose_name = 'Profissional'
+        verbose_name_plural = 'Profissionais'
+
+    def __str__(self):
+        nome = self.nome_social if self.nome_social else self.nome
+        return f'{nome} - {self.especialidade}'
+
+class Consulta(BaseModel):
+    paciente = models.CharField(max_length=100, blank=False, verbose_name='Nome do paciente')
+    pac_nome_social = models.CharField(max_length=100, blank=True, verbose_name='Nome social do paciente')
+    data_consulta = models.DateField(blank=False, verbose_name='Data da consulta')
+    profissional = models.ForeignKey(Profissional, related_name='profissional', verbose_name='Nome do profissional', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Consulta'
+        verbose_name_plural = 'Consultas'
+
